4054f9ba2838481d4399888b91aba346b1374001
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-07-23 22:53:50 -0400

    Added the new Lifecycle project which contains basic interfaces which will in the future interact with the Application class (also in Lifecycle) to act on activation, suspension, navigation, etc.

---
+++ BassClefStudio.UWP.Lifecycle/Application.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Core;
+
+namespace BassClefStudio.UWP.Lifecycle
+{
+    public abstract class Application : Windows.UI.Xaml.Application
+    {
+        public Application()
+        {
+            ////Register system events
+            this.Suspending += OnSuspending;
+            SystemNavigationManager.GetForCurrentView().BackRequested += BackRequested;
+        }
+
+        private void BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnSuspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnBackgroundActivated(BackgroundActivatedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BassClefStudio.UWP.Lifecycle/ILifecycleHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BassClefStudio.UWP.Lifecycle
+{
+    /// <summary>
+    /// Represents a service that can manage an <see cref="Application"/>'s back navigation.
+    /// </summary>
+    public interface IBackHandler
+    {
+        /// <summary>
+        /// A method that is called whenever the <see cref="Application"/> receieves a back navigation request.
+        /// </summary>
+        void BackRequested();
+    }
+
+    /// <summary>
+    /// Represents a service that can manage an <see cref="Application"/>'s foreground activation.
+    /// </summary>
+    public interface IForegroundActivationHandler
+    {
+        /// <summary>
+        /// A method that is called whenever the <see cref="Application"/> is activated in the foreground - i.e., by a user launching the application, through a file or URI scheme, or the like.
+        /// </summary>
+        void ForegroundActivated();
+    }
+
+    /// <summary>
+    /// Represents a service that can manage an <see cref="Application"/>'s background activation.
+    /// </summary>
+    public interface IBackgroundActivationHandler
+    {
+        /// <summary>
+        /// An asynchronous <see cref="Task"/> that is called whenever the <see cref="Application"/> is activated in the background - i.e., there is no UI showing. Here, the <see cref="ILifecycleHandler"/> can act on background tasks and app service requests.
+        /// </summary>
+        Task BackgroundActivated();
+    }
+
+    /// <summary>
+    /// Represents a service that can manage an <see cref="Application"/>'s suspension from the foreground.
+    /// </summary>
+    public interface ISuspendingHandler
+    {
+        /// <summary>
+        /// A method that is called whenever a foreground-activated <see cref="Application"/> is closing or returning to the background. Here, the <see cref="ILifecycleHandler"/> can dispose or broker resources before the <see cref="Application"/> has fully closed.
+        /// </summary>
+        void OnSuspending();
+    }
+}
