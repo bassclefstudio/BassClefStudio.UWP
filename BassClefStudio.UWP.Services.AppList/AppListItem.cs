8da783b05a5cd1bcde10a68b72c46853426badea
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-08-22 14:17:46 -0400

    Added a new BassClefStudio.UWP.Services.AppList project to the solution.

---
+++ BassClefStudio.UWP.Services.AppList/AppListItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BassClefStudio.UWP.Services.AppList
+{
+    public class AppListItem
+    {
+    }
+}
---
+++ BassClefStudio.UWP.Services.AppList/IAppListProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BassClefStudio.UWP.Services.AppList
+{
+    /// <summary>
+    /// Represents a service that can get and manage a list of installed applications on the system (in the form of their <see cref="AppListItem"/> entries).
+    /// </summary>
+    public interface IAppListProvider
+    {
+        /// <summary>
+        /// An <see cref="ObservableCollection{T}"/> of <see cref="AppListItem"/>s for every application installed on the system.
+        /// </summary>
+        ObservableCollection<AppListItem> Applications { get; }
+
+        /// <summary>
+        /// Asynchronously updates the <see cref="Applications"/> collection.
+        /// </summary>
+        Task Update();
+    }
+}
