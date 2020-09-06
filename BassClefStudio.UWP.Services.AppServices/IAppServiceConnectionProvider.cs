cfe00cfcb100e2f5eb4a9537f9338e92395df76f
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-09-06 16:12:15 -0400

    Updated Services.AppServices package with extension methods.

--- BassClefStudio.UWP.Services.AppServices/IAppServiceConnectionProvider.cs
+++ BassClefStudio.UWP.Services.AppServices/IAppServiceConnectionProvider.cs
@@ -42,4 +42,25 @@ namespace BassClefStudio.UWP.Services.AppServices
         public AppServiceConnectionException(string message) : base(message) { }
         public AppServiceConnectionException(string message, Exception inner) : base(message, inner) { }
     }
+
+    public static class AppServiceConnectionProviderExtensions
+    {
+        /// <summary>
+        /// Connects to an app service as specified by the given <see cref="AppServiceConnectionInfo"/>.
+        /// </summary>
+        /// <param name="info">The information about the connection.</param>
+        public static async Task<IAppServiceConnectionProvider> Connect(this IEnumerable<IAppServiceConnectionProvider> providers, AppServiceConnectionInfo info)
+        {
+            var provider = providers.FirstOrDefault(p => p.CanConnect(info));
+            if(provider != null)
+            {
+                await provider.Connect(info);
+                return provider;
+            }
+            else
+            {
+                throw new AppServiceConnectionException("No IAppServiceConnectionProviders could connect using the given connection info.");
+            }
+        }
+    }
 }
