59ab4c480754d3d19cff9b9292ec68b8f5bccb66
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-10-18 17:43:42 -0400

    Added co-ordinate extension methods, related to the way in which the IDialService method returns the dial's position and bounds (but applicable elsewhere).

---
+++ BassClefStudio.UWP.Services.Views/CoordinateExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace BassClefStudio.UWP.Services.Views
+{
+    /// <summary>
+    /// Provides extension methods on the <see cref="Windows.Foundation.Point"/> struct that deal with relative co-ordinates.
+    /// </summary>
+    public static class CoordinateExtensions
+    {
+        /// <summary>
+        /// Transforms a <see cref="Point"/> relative to the <see cref="UIElement"/> into a <see cref="Point"/> relative to the <see cref="Window"/>.
+        /// </summary>
+        /// <param name="element">The <see cref="UIElement"/> that <paramref name="point"/> is relative to.</param>
+        /// <param name="point">The <see cref="Point"/> to transform.</param>
+        public static Point GetWindowCoordinate(this UIElement element, Point point)
+        {
+            GeneralTransform transform = element.TransformToVisual(Window.Current.Content);
+            return transform.TransformPoint(point);
+        }
+
+        /// <summary>
+        /// Transforms a <see cref="Point"/> relative to the <see cref="Window"/> into a <see cref="Point"/> relative to the <see cref="UIElement"/>.
+        /// </summary>
+        /// <param name="element">The <see cref="UIElement"/> that <paramref name="point"/> is relative to.</param>
+        /// <param name="point">The <see cref="Point"/> to transform.</param>
+        public static Point GetElementCoordinate(this UIElement element, Point point)
+        {
+            GeneralTransform transform = Window.Current.Content.TransformToVisual(element);
+            return transform.TransformPoint(point);
+        }
+    }
+}
